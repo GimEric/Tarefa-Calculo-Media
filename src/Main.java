d9f5aee7c8e3b6571c81e3a34210190c464bd9c6
eric gimenes <gimenes.eric@live.com>
2023-12-27 16:33:34 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        double num1 = 10;
+        double num2 = 5;
+        double num3 = 8;
+        double num4 = 4;
+        double  media = (num1 + num2 + num3 + num4) /4 ;
+
+        System.out.println(media);
+    }
+
+
+
+
+
+}
+
+
